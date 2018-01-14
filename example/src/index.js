cd621b625acf249b6b7f40f7abd3792ec94ac24c
Daniel Huisman <daniel@huisman.me>
2018-01-14 19:04:33 +0100

    Add missing example code

--- example/src/index.js
+++ example/src/index.js
@@ -2,5 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Root from './Root';
+import {authors} from './actions';
+import store from './store';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
+
+(async () => {
+    await store.dispatch(authors.getAll({
+        query: {
+            page: {
+                number: 1,
+                size: 100
+            }
+        }
+    }));
+    await store.dispatch(authors.getRelationship({
+        id: '1',
+        relationship: 'books'
+    }));
+})();
